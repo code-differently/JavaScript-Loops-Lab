55a7b9396551dc56bfce469356ebb8014e569476
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-11-23 23:32:29 -0500

    Couple steps done

---
+++ script.js
@@ -0,0 +1,45 @@
+// Step 1
+
+for (i = 1; i <=10; i++){
+    console.log(i);
+}
+
+// Step 2
+
+for (i=1; i <=10; i++){
+    console.log(i*i);
+}
+
+// Step 3
+
+for (n = 20; n > 0; n-=2){
+    console.log(n);
+}
+// Step 4
+
+
+// Step 5
+
+let user = prompt("Are we there yet?")
+if (user == "Yes"){
+    console.log("Good!")
+} else{
+    while(user != "Yes"){
+        prompt("Are we there yet?")
+    }
+}
+    
+
+
+// Step 6
+
+let output = "";
+for (let i = 1; i < 10; i++){
+    output += i;
+    console.log(output);
+}
+
+// Step 7
+
+
+// Step 8
