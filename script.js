a9735c583662dd55807d2761b0a5a11525bcaac3
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-11-25 18:36:58 -0500

    Finished all steps

--- script.js
+++ script.js
@@ -12,31 +12,34 @@ for (i=1; i <=10; i++){
 
 // Step 3
 
-for (n = 2; n >= 20; n+=2){
-    console.log(n);
+for (let j = 2; j <= 20; j+= 2){
+    console.log(j);
 }
-// Step 4
-let sum = 0;
 
-for (n = 5; n < 10; n++){
-       n += sum;
-       console.log(sum)
+// Step 4
+let num1= prompt("Enter a random number")
+let num2= prompt("Enter a bigger random number")
+
+for(i = num1; i < num2; i++){
+    if(num1 > num2){
+        prompt("Try again!");
+    }let i = num1 + num2;
+    console.log(i);
 }
 
 
 // Step 5
 
-let user = prompt("Are we there yet?")
-let correctAnswer= "Yes"
+do {
+    user= prompt("Are we there yet?")
+}while(user != "Yes")
 
-
-    
-
+console.log("Good!")
 
 // Step 6
 
 let output = "";
-for (let i = 1; i < 10; i++){
+for (let i = 1; i < 6; i++){
     output += i;
     console.log(output);
 }
@@ -60,7 +63,6 @@ let answer= '';
 for(i = 1; i <= 8; i++){
     for(j = 1; j <= 8; j++){
             for(k = " | "; k <= 8; k++){
-
             }
         answer += (i * j + k);
     }
