70faf8ae11bbbe46cfe6348ea3f86c72ae6ad7ce
CWilliams75 <coreywllms9@gmail.com>
2020-11-29 21:51:04 -0500

    problem 2 - 6

--- loops.js
+++ loops.js
@@ -1,3 +1,34 @@
+/*Problem 1
+for (i = 1; i <= 10; i++){
+    console.log(i)*/
+
+/*Problem 2
 for (let i = 1; i <=10; i++) {
-    console.log (i)
-}
+     console.log(i * i); 
+}*/
+
+/*Problem 3
+let n = 20
+    for (i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            console.log(i)
+    }
+}*/
+
+/*Problem 5
+do {
+    user = prompt("Are we there yet")
+}while (user !== "Yes")
+alert("Good!") */
+    
+
+
+//Problem 6
+
+/*for (let i = 1; i <= 5; i++) {
+    let stars = ""
+    for (let j = 1; j <= i; j++) 
+        stars += "*";
+    
+    console.log(stars);
+}*/
